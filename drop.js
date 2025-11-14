9083728655d9f5ed75ec3bf68d9088097f6abba2
Michal <31184485+MichalKuracina@users.noreply.github.com>
2025-11-14 17:08:32 +0100

    done

--- drop.js
+++ drop.js
@@ -1,19 +1,37 @@
 class Drop {
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.velocity;
+    constructor(x, height, depth) {
+        this.startingX = x;
+        this.x = this.startingX;
+        this.height = height;
+        this.depth = depth;
+        this.startVelocity = this.remap(this.depth, 1, 20, 5, 15);
+        this.velocity = this.startVelocity;
+        this.y = this.height;
+        this.dropLength = this.remap(this.depth, 1, 20, 5, 30);
+        this.dropStrokeWeight = this.remap(this.depth, 1, 20, 1, 4);
+        this.dropDirection = random(2, 3);
     }
 
     update() {
-        this.y = this.y + 1;
+        this.x += 1;
+        this.y = this.y + this.velocity;
+        this.velocity += this.remap(this.depth, 1, 20, 0.03, 0.1); // gravity effect
+
+        if (this.y > height) {
+            this.y = this.height;
+            this.velocity = this.startVelocity;
+            this.x = this.startingX;
+        }
     }
 
     show() {
         stroke(138, 43, 226);
-        strokeWeight(5);
+        strokeWeight(this.dropStrokeWeight);
 
-        line(this.x, this.y, this.x, this.y + 10);
+        line(this.x, this.y, this.x + this.dropDirection, this.y + this.dropLength);
+    }
+
+    remap(value, start1, stop1, start2, stop2) {
+        return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
     }
 }
--- sketch.js
+++ sketch.js
@@ -3,11 +3,16 @@ let drops = [];
 function setup() {
     createCanvas(800, 600);
 
-    drops.push(new Drop(400, 0, 0));
+    for (let i = 0; i < 500; i++) {
+        let startingX = random(-width / 2, width);
+        let startingY = random(-height, 0);
+        let depth = random(1, 20);
+        drops.push(new Drop(startingX, startingY, depth));
+    }
 }
 
 function draw() {
-    background(230, 230, 250);
+    background(243, 212, 255);
 
     drops.forEach((drop) => {
         drop.update();
